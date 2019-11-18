1e42c05ea06495252f92e24b87f070ceef46c687
IamWangYunKai <615349118@163.com>
2019-11-18 13:58:33 +0800

    add camera calibration

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # RoBoCar
+This repository is for the robot car development, including main program, sensors test scripts, auto install scripts, helpful tools, etc.
+#### Sensors used:
+* LiDAR: [Velodyne VLP-16](https://www.velodynelidar.com/vlp-16.html)
+* Camera: [MYNT EYE D-1000-120](https://www.myntai.com/mynteye/depth)
+* GPS: [千寻 D300-GNSS](https://mall.qxwz.com/market/products/details?name=ouabiwv7762375598)
+* IMU： [Xsens MTi-3](https://shop.xsens.com/shop/mti-1-series/mti-3-ahrs/mti-3)
+
+
+# Features
+* None
+
+
+# Test List
+- [ ] camera image reading in python with pybind11
+- [ ] camera calibration
+- [ ] LiDAR data reading in python
+- [ ] CAN bus test with robot car
+- [ ] auto install scripts on ubuntu16.04/18.04
+
+# TODO List
+- [ ] GPS data reading
+- [ ] IMU data reading
---
+++ test/camera/calibration/calibration.py
@@ -0,0 +1,60 @@
+import cv2
+import glob
+import numpy as np
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+img = cv2.imread('left12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+# undistort
+mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    tot_error += error
+
+print("total error: ", mean_error/len(objpoints))
