f33d51530198b51f2e7c788618d711959cdd53ec
IamWangYunKai <615349118@163.com>
2020-01-06 23:34:03 +0800

    update sensor manager

--- utils.py
+++ utils.py
@@ -7,6 +7,14 @@
 @e-mail: wangyunkai@zju.edu.cn
 """
 
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kw):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(cls)  
+        return cls._instance 
+    
 def debug(info, info_type='debug'):
 	if info_type == 'error':
 		print('\033[1;31m ERROR:', info, '\033[0m')
