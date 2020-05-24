bfd2a60f4f5bf34f8575c269395f35155af7d22c
IamWangYunKai <615349118@163.com>
2020-05-24 19:55:04 +0800

    add filter in gps

--- gps/gps.py
+++ gps/gps.py
@@ -1,12 +1,80 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import math
+import time
 import serial
 
+class Filter():
+    def __init__(self):
+        self.v = 0.0
+        self.yaw = 0.0
+
+        self.x = 0.0
+        self.y = 0.0
+        self.last_t = time.time()
+        self.cnt = 0
+        self.INIT_STEPS = 50
+        self.ALPHA = 0.2
+        self.MAX_V = 15.0
+        
+        self.x_his = []
+        self.y_his = []
+        
+        self.x_bias = 0.0
+        self.y_bias = 0.0
+        
+    
+    def dist(self, x, y):
+        return math.sqrt((self.x-x)**2 + (self.y-y)**2)
+        
+    def step(self, x, y):
+        if self.cnt >= self.INIT_STEPS:
+            x = x - self.x_bias
+            y = y - self.y_bias
+            
+        self.cnt += 1
+        now = time.time()
+
+        d = self.dist(x, y)
+        dt = now - self.last_t
+        if dt < 0.0001: dt = 0.1
+        v = d/dt
+        
+        if self.cnt < self.INIT_STEPS-10:
+            self.x = (1-self.ALPHA)*self.x + self.ALPHA*x
+            self.y = (1-self.ALPHA)*self.y + self.ALPHA*y
+            self.x_bias = self.x
+            self.y_bias = self.y
+            
+        elif self.cnt < self.INIT_STEPS:
+            self.x = (1-self.ALPHA)*self.x + self.ALPHA*x
+            self.y = (1-self.ALPHA)*self.y + self.ALPHA*y
+            self.x_his.append(x)
+            self.y_his.append(y)
+        else:
+            if v > self.MAX_V:
+                self.x = self.x + math.cos(self.yaw)*self.v*dt
+                self.y = self.y + math.sin(self.yaw)*self.v*dt
+            else:
+                self.v = (1-self.ALPHA)*self.v + self.ALPHA*v
+                self.x = x
+                self.y = y
+                
+                self.yaw = math.atan2((y-self.y_his[5]), (x-self.y_his[5]))
+        
+            self.x_his.pop(0)
+            self.y_his.pop(0)
+            self.x_his.append(self.x)
+            self.y_his.append(self.y)
+            self.last_t = now
+        
+        return self.x, self.y, self.v
+    
 class GPS():
     def __init__(self, port = '/dev/ttyUSB0'):
         self.serial = None
         self.port = port
+        self.filter = Filter()
     
     def start(self):
         try:
@@ -19,7 +87,10 @@ class GPS():
         
     def get(self):
         data = self.serial.readline()
-        latitude,longtitude = self.parseGPS(data)
+        while data[:6] != '$GPGGA':
+            data = self.serial.readline()
+        x, y, v = self.parseGPS(data)
+        return x, y, v
     
     def parseGPS(self, line):
         try:
@@ -27,22 +98,26 @@ class GPS():
             #print(data)
             latitude = data[2]	
             longtitude = data[4]
-        except:
-            print('Error when parse GPS:', data)
+            #num_star = data[7]
+            #hdop = data[8]
+            #convert degree+minute to degree
+        	#lantitude: DDmm.mm
+        	#longtitude: DDDmm.mm
+            lan_degree = latitude[:2]
+            lan_minute = latitude[2:]
+            latitude = float(lan_degree) + float(lan_minute)/60
+        
+            long_degree = longtitude[:3]
+            long_minute = longtitude[3:]
+            longtitude = float(long_degree) + float(long_minute)/60
             
-        #num_star = data[7]
-        #hdop = data[8]
-        #convert degree+minute to degree
-    	#lantitude: DDmm.mm
-    	#longtitude: DDDmm.mm
-        lan_degree = latitude[:2]
-        lan_minute = latitude[2:]
-        latitude = float(lan_degree) + float(lan_minute)/60
-    
-        long_degree = longtitude[:3]
-        long_minute = longtitude[3:]
-        longtitude = float(long_degree) + float(long_minute)/60
-        return latitude,longtitude
+            x, y = self.gps2xy(self, latitude, longtitude)
+            filter_x, filter_y, filter_v = self.filter.step(x, y)
+            return filter_x, filter_y, filter_v
+        except:
+            #print('Error when parse GPS:', data)
+            return 0.0, 0.0
+        
     
     def gps2xy(self, latitude, longtitude):
     
