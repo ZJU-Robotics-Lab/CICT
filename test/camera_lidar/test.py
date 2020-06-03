3623ad8a9b298fc0b474d706aa172ebce53a558a
IamWangYunKai <615349118@163.com>
2020-06-03 15:04:32 +0800

    finish cmd generate

--- test/camera_lidar/test.py
+++ test/camera_lidar/test.py
@@ -152,6 +152,9 @@ point_cloud = np.dot(pitch_rotationMat, point_cloud)
 rotationMat = np.dot(rotationMat, np.linalg.inv(pitch_rotationMat))
 translationMat = np.dot(translationMat, np.linalg.inv(pitch_rotationMat))
 
+point_size = 1
+thickness = 4
+    
 def lidar2camera(point_cloud, rotationMat, translationMat):
     trans_pc = np.dot(rotationMat, point_cloud) + np.tile(translationMat, (point_cloud.shape[1], 1)).T
 
@@ -161,8 +164,7 @@ def lidar2camera(point_cloud, rotationMat, translationMat):
             ])
     
     
-    point_size = 1
-    thickness = 4
+
     for i in range(image_uv.shape[1]):
         point = (int(image_uv[0][i]), int(image_uv[1][i]))
         if point[0] > width or point[0] < 0 or point[1] > height or point[1] < 0:
@@ -221,51 +223,121 @@ def camera2lidar(image_uv):
     #v.animation()
     
     
-pix_width = 0.05
+pix_width = 0.04
 map_x_min = -2.0
 map_x_max = 10.0
 map_y_min = -10.0
 map_y_max = 10.0
 lim_z = -1.5
-        
+
+width = int((map_x_max-map_x_min)/pix_width)
+height = int((map_y_max-map_y_min)/pix_width)
+    
+u_bias = int(np.abs(map_y_max)/(np.abs(map_y_max)+np.abs(map_y_min))*height)
+v_bias = int(np.abs(map_x_max)/(np.abs(map_x_max)+np.abs(map_x_min))*width)
+
+def normalize(angle):
+    while angle >= np.pi or angle <= -np.pi:
+        if angle >= np.pi:
+            angle -= np.pi
+        else:
+            angle += np.pi
+    return angle
+
+def xy2uv(x, y):
+    u = -int(x/pix_width) + u_bias
+    v = -int(y/pix_width) + v_bias
+    return u, v
+    
 def get_cost_map(trans_pc, point_cloud):
-    width = int((map_x_max-map_x_min)/pix_width)
-    height = int((map_y_max-map_y_min)/pix_width)
+    
     img = np.zeros((width,height,1), np.uint8)
-    img.fill(127)
+    img.fill(0)
+    
+    img2 = np.zeros((width,height,1), np.uint8)
+    img2.fill(255)
     
     x = []
     y = []
           
-    for i in range(point_cloud.shape[1]):
-        if point_cloud[2][i] < lim_z or point_cloud[0][i] < map_x_min or point_cloud[0][i] > map_x_max or point_cloud[1][i] < map_y_min or point_cloud[1][i] > map_y_max:
-            continue
-        u = -int((point_cloud[0][i]-map_x_min)/pix_width) #+ int(width/2)
-        v = -int((point_cloud[1][i]-map_y_min)/pix_width) #+ int(height/2)
-        img[u][v] = 0
-
-        x.append(u)
-        y.append(v)
         
     kernel = np.ones((5,5),np.uint8)  
     img = cv2.erode(img,kernel,iterations = 1)
-    #cv2.imshow('image', img)    
+    #cv2.imshow('image', img)
+
     for i in range(trans_pc.shape[1]):
         if trans_pc[0][i] < map_x_min or trans_pc[0][i] > map_x_max or trans_pc[1][i] < map_y_min or trans_pc[1][i] > map_y_max:
             continue
-        u = -int((trans_pc[0][i]-map_x_min)/pix_width)# + int(width/2)
-        v = -int((trans_pc[1][i]-map_y_min)/pix_width)# + int(height/2)
+
+        u, v = xy2uv(trans_pc[0][i], trans_pc[1][i])
         img[u][v] = 255
 
         x.append(u)
         y.append(v)
-        
-        
-    #kernel_size = (3, 3)
-    #sigma = 15
-    #img = cv2.GaussianBlur(img, kernel_size, sigma)
-    cv2.imshow('image', img)  
+    
+    for i in range(point_cloud.shape[1]):
+        if point_cloud[2][i] < lim_z or point_cloud[0][i] < map_x_min or point_cloud[0][i] > map_x_max or point_cloud[1][i] < map_y_min or point_cloud[1][i] > map_y_max:
+            continue
 
+        u, v = xy2uv(point_cloud[0][i], point_cloud[1][i])
+        img2[u][v] = 0
+
+        x.append(u)
+        y.append(v)
+        
+    kernel = (5,5)
+    img = cv2.blur(img, kernel)
+    img2 = cv2.blur(img2, kernel)
+    
+    img = cv2.addWeighted(img,0.5,img2,0.5,0)
+    
+    vel = 1.0
+    L = 5.0
+    n = 100
+    m = 20
+    max_theta = 2*np.pi/3
+    rs = []
+    for i in range(m):
+        theta = i*2.0*max_theta/m - max_theta
+        if np.abs(theta - 0.) < 0.00001:
+            rs.append(9999999999999)
+            continue
+        r = L/theta
+        rs.append(r)
+        rs.append(-r)
+
+    best_cost = 0
+    best_r = None
+    best_u = []
+    best_v = []
+    for r in rs:
+        #w = v/r
+        theta = L/np.abs(r)
+        us = []
+        vs = []
+        cost = 0
+        for i in range(n):
+            x = np.abs(r*np.sin(i*theta/n))
+            y = r*(1-np.cos(i*theta/n))
+            u, v = xy2uv(x, y)
+            us.append(u)
+            vs.append(v)
+            cost += img[u][v]/255.0
+            #img[u][v] = 0
+            
+        if best_cost < cost:
+            best_cost = cost
+            best_r = r
+            best_u = us
+            best_v = vs
+            
+    for i in range(len(best_u)):
+        u = best_u[i]
+        v = best_v[i]
+        img[u][v] = 0
+
+    print('R:', best_r, '\tV:', vel, '\tW:', vel/best_r)
+    cv2.imshow('Result', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
